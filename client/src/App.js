39124b22b8a92cd5cb69d2072d742a35eb81785f
kg-kartik <goel.kartik39@gmail.com>
2021-01-17 00:22:55 +0530

    Basic apollo-client setup

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import LinkList from "./components/LinkList";
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1> hello </h1>
+                <LinkList />
+            </div>
+        );
+    }
+}
+
+export default App;
